a7a476672ffb3067bfe11d7202c6bfa100f5ad01
Bob Petersen <33petersen@gmail.com>
2018-08-07 11:43:35 -0500

    cleaned up code in bracket_matching

--- bracket_matching/bracket_matching.rb
+++ bracket_matching/bracket_matching.rb
@@ -1,21 +1,36 @@
-class Brackets    # creates an object of the type class
-  BRACKETS = {    # defines what opening and closing brackets are
-    '(' => ')',   # key: value,
+# returns the class of object
+# defines what opening and closing brackets are
+# key: value,
+class Brackets
+  BRACKETS = {
+    '(' => ')',
     '[' => ']',
     '{' => '}'
-  }
+  }.freeze
 
-  def self.paired?(string)                    # self defines the method bracket. (string) is the argument it can accept
-    stack = []                                # the stack (this is not an array)(think of it as verticle, you can only access the top or last)
+  # self defines the method bracket. (string) is the argument it can accept
+  # the stack (this is not an array)
+  # (think of it as verticle, you can only access the top or last)
 
-    string.chars.each do |char|               # loops through each character
-      if BRACKETS.keys.include?(char)         # if statement to check if it includes a key(opening) bracket
-        stack.push(char)                      # if it does it will push it to the stack
-      elsif BRACKETS.values.include?(char)    
-        return false unless BRACKETS[stack.pop] == char  # checks the value(closing) of bracket if it's matches it will 
-      end                                                # pop the openeing bracket off the top of the stack
+  def self.paired?(string)
+    stack = []
+
+    # loops through each character
+    # if statement to check if it includes a key(opening) bracket
+    # if it does it will push it to the stack
+
+    string.chars.each do |char|
+      if BRACKETS.key?.include?(char)
+        stack.push(char)
+      elsif BRACKETS.value?.include?(char)
+
+        # checks the value(closing) of bracket if it's matches it will
+        # pop the openeing bracket off the top of the stack
+
+        return false unless BRACKETS[stack.pop] == char
+      end
     end
-    stack.empty?  # if the stack is empty it will return true
+    stack.empty? # if the stack is empty it will return true
   end
 end
 
