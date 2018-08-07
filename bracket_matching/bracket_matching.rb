e63420ebd4b5b97c2d8442f390a7bba870d76ac0
Bob Petersen <33petersen@gmail.com>
2018-08-07 13:15:54 -0500

    completed

--- bracket_matching/bracket_matching.rb
+++ bracket_matching/bracket_matching.rb
@@ -7,30 +7,25 @@ class Brackets
     '[' => ']',
     '{' => '}'
   }.freeze
-
   # self defines the method bracket. (string) is the argument it can accept
   # the stack (think of it as a vertical array in which
   # you can only access the top or last)
-
   def self.paired?(string)
     stack = []
-
     # loops through each character
     # if statement to check includes a key(opening) bracket
     # it will push it to the stack
-
     string.chars.each do |char|
       if BRACKETS.key?.include?(char)
         stack.push(char)
       elsif BRACKETS.value?.include?(char)
-
         # checks the value(closing) of bracket if it's matches it will
         # pop the openeing bracket off the top of the stack
-
         return false unless BRACKETS[stack.pop] == char
       end
     end
-    stack.empty? # if the stack is empty it will return true
+    # if the stack is empty it will return true
+    stack.empty?
   end
 end
 
